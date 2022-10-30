855b4ac7a0700fe3e69f54a0a22b24302da5a712
ogoshielfee <elfee244@yahoo.co.jp>
2022-10-30 18:02:02 +0900

    “todotutorial”

--- lib/main.dart
+++ lib/main.dart
@@ -31,128 +31,153 @@ class MyHomePage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref){
 
     final List<Task> taskList = ref.watch(tasksProvider);
-    bool editChange = false;
+    final editChange = ref.watch(editProvider.state);
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('ToDoリスト'),
         actions: [
-          Consumer(
-            builder: (context, ref, _) {
-              return IconButton(
-                onPressed: (){editChange != editChange;},
-                icon: Icon(Icons.edit)
-              );
-            }
-          )
+          IconButton(
+            onPressed: (){ref.read(editProvider.notifier).state = !ref.read(editProvider.notifier).state;},
+            icon: editChange.state == false?const Icon(Icons.edit):const Icon(Icons.check),
+          ),
         ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            editChange == true?Card(
-              child: ListTile(
-                leading: Icon(Icons.add),
-                title: Text('タスクを追加'),
-                onTap: ()=> showDialog<String>(
-                    context: context,
-                    builder: (BuildContext context){
-                      String appendTask = '';
-                      return Consumer(
-                        builder: (context, ref, _){
-                          return AlertDialog(
-                            title: const Text('タスクを追加'),
-                            content: SizedBox(
-                              child: TextField(
-                                onChanged: (value){
-                                  appendTask = value;
-                                },
-                                decoration: const InputDecoration(border: OutlineInputBorder()),
-                              ),
+        child: ReorderableListView(
+          header: editChange.state == false?null:ListTile(
+            onTap: ()=> showDialog<String>(
+              context: context,
+              builder: (BuildContext context) {
+                String title = '';
+                return Consumer(
+                    builder: (context, ref, _) {
+                    return AlertDialog(
+                      title: const Text('タスクを追加'),
+                      content: SizedBox(
+                        child: TextField(
+                          onChanged: (value){
+                            title = value;
+                          },
+                          decoration: const InputDecoration(labelText: 'タスク名'),
+                        ),
+                      ),
+                      actions: <Widget>[
+                        TextButton(
+                          onPressed: () => Navigator.pop(context, 'Cancel'),
+                          child: const Text('Cancel'),
+                        ),
+                        TextButton(
+                          onPressed: (){
+                            ref.read(tasksProvider.notifier).insertTask(
+                                Task(id: DateTime.now().millisecondsSinceEpoch, title: title, isDone: false)
+                            );
+                            Navigator.pop(context, 'OK');
+                          },
+                          child: const Text('OK'),
+                        ),
+                      ],
+                    );
+                  }
+                );
+              }
+            ),
+            leading: const Icon(Icons.add),
+            title: const Text('タスクを追加'),
+          ),
+          onReorder:(oldIndex, newIndex) {
+            ref.read(tasksProvider.notifier).dragAndDrop(oldIndex, newIndex);
+          },
+          footer: editChange.state == false?null:ListTile(
+            onTap: ()=> showDialog<String>(
+                context: context,
+                builder: (BuildContext context) {
+                  String title = '';
+                  return Consumer(
+                      builder: (context, ref, _) {
+                        return AlertDialog(
+                          title: const Text('タスクを追加'),
+                          content: SizedBox(
+                            child: TextField(
+                              onChanged: (value){
+                                title = value;
+                              },
+                              decoration: const InputDecoration(labelText: 'タスク名'),
                             ),
-                            actions: <Widget>[
-                              TextButton(
-                                onPressed: () => Navigator.pop(context, 'Cancel'),
-                                child: const Text('Cancel'),
-                              ),
-                              TextButton(
-                                onPressed: (){
-                                  ref.read(tasksProvider.notifier).addTask(
-                                      Task(id: DateTime.now().millisecondsSinceEpoch, title: appendTask, isDone: false)
-                                  );
-                                  Navigator.pop(context, 'OK');
-                                },
-                                child: const Text('OK'),
-                              ),
-                            ],
-                          );
-                        },
-                      );
-                    }
-                ),
-              ),
-            ):Container(),
-            Expanded(
-              child: ReorderableListView(
-                onReorder:(oldIndex, newIndex) {
-                  ref.read(tasksProvider.notifier).dragAndDrop(oldIndex, newIndex);
-                },
-                children: taskList.map((task) =>
-                  Card(
-                    key: Key(task.title),
-                    child: ListTile(
-                      onTap: (){ref.read(tasksProvider.notifier).toggleDone(task.id);},
-                      title: Text(task.title, style: task.isDone == true?const TextStyle(decoration: TextDecoration.lineThrough):null,),
-                      trailing: task.isDone == true?const Icon(Icons.done):null,
-                    ),
-                  ),
-                ).toList(),
+                          ),
+                          actions: <Widget>[
+                            TextButton(
+                              onPressed: () => Navigator.pop(context, 'Cancel'),
+                              child: const Text('Cancel'),
+                            ),
+                            TextButton(
+                              onPressed: (){
+                                ref.read(tasksProvider.notifier).addTask(
+                                    Task(id: DateTime.now().millisecondsSinceEpoch, title: title, isDone: false)
+                                );
+                                Navigator.pop(context, 'OK');
+                              },
+                              child: const Text('OK'),
+                            ),
+                          ],
+                        );
+                      }
+                  );
+                }
+            ),
+            leading: const Icon(Icons.add),
+            title: const Text('タスクを追加'),
+          ),
+          children: taskList.map((task) =>
+            Card(
+              key: Key(task.title),
+              child: ListTile(
+                onTap: (){editChange.state == false?
+                  ref.read(tasksProvider.notifier).toggleDone(task.id):
+                  showDialog<String>(
+                      context: context,
+                      builder: (BuildContext context) {
+                        String title = '';
+                        return Consumer(
+                            builder: (context, ref, _) {
+                              return AlertDialog(
+                                title: const Text('タスクの編集'),
+                                content: SizedBox(
+                                  child: TextField(
+                                    onChanged: (value){
+                                      title = value;
+                                    },
+                                    decoration: const InputDecoration(labelText: 'タスク名'),
+                                  ),
+                                ),
+                                actions: <Widget>[
+                                  TextButton(
+                                    onPressed: () => Navigator.pop(context, 'Cancel'),
+                                    child: const Text('Cancel'),
+                                  ),
+                                  TextButton(
+                                    onPressed: (){
+                                      ref.read(tasksProvider.notifier).editTask(task.id, title);
+                                      Navigator.pop(context, 'OK');
+                                    },
+                                    child: const Text('OK'),
+                                  ),
+                                ],
+                              );
+                            }
+                        );
+                      }
+                  );},
+                title: Text(task.title, style: task.isDone == true?const TextStyle(decoration: TextDecoration.lineThrough):null,),
+                trailing: editChange.state == true?
+                IconButton(
+                  onPressed: (){ref.read(tasksProvider.notifier).removeTask(task.id);},
+                  icon: Icon(Icons.clear),
+                ): task.isDone == true?const Icon(Icons.done):null
               ),
             ),
-            editChange == true?Card(
-              child: ListTile(
-                leading: Icon(Icons.add),
-                title: Text('タスクを追加'),
-                onTap: ()=> showDialog<String>(
-                    context: context,
-                    builder: (BuildContext context){
-                      String appendTask = '';
-                      return Consumer(
-                        builder: (context, ref, _){
-                          return AlertDialog(
-                            title: const Text('タスクを追加'),
-                            content: SizedBox(
-                              child: TextField(
-                                onChanged: (value){
-                                  appendTask = value;
-                                },
-                                decoration: const InputDecoration(border: OutlineInputBorder()),
-                              ),
-                            ),
-                            actions: <Widget>[
-                              TextButton(
-                                onPressed: () => Navigator.pop(context, 'Cancel'),
-                                child: const Text('Cancel'),
-                              ),
-                              TextButton(
-                                onPressed: (){
-                                  ref.read(tasksProvider.notifier).addTask(
-                                      Task(id: DateTime.now().millisecondsSinceEpoch, title: appendTask, isDone: false)
-                                  );
-                                  Navigator.pop(context, 'OK');
-                                },
-                                child: const Text('OK'),
-                              ),
-                            ],
-                          );
-                        },
-                      );
-                    }
-                ),
-              ),
-            ):Container(),
-          ],
+          ).toList(),
         ),
       ),
     );
--- lib/providers.dart
+++ lib/providers.dart
@@ -7,6 +7,8 @@ const List<Task> tasksList = [
   Task(id: 2, title: 'task3', isDone: false),
 ];
 
+final editProvider = StateProvider<bool>((ref) => false);
+
 class TasksNotifier extends StateNotifier<List<Task>> {
   TasksNotifier(): super(tasksList);
 
@@ -16,6 +18,37 @@ class TasksNotifier extends StateNotifier<List<Task>> {
       newState.add(task);
     }
     newState.add(newTask);
+    state = newState;
+  }
+
+  void insertTask(Task newTask) {
+    List<Task> newState = [];
+    for (final task in state){
+      newState.add(task);
+    }
+    newState.insert(0,newTask);
+    state = newState;
+  }
+
+  void removeTask(int id) {
+    List<Task> newState = [];
+    for (final task in state){
+      if(task.id != id){
+        newState.add(task);
+      }
+    }
+    state = newState;
+  }
+
+  void editTask(int id, String title) {
+    List<Task> newState = [];
+    for (final task in state) {
+      if (task.id == id) {
+        newState.add(task.copyWith(title: title));
+      } else {
+        newState.add(task);
+      }
+    }
     state = newState;
   }
 
